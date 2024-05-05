af916bc288cbad1baeab37913fd336b9be5c8c03
ardans <ardans@foxmail.com>
2024-05-05 18:53:58 +0800

    🦄 refactor: remove console.log

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -12,7 +12,6 @@ cli.command('template', 'Create a new project from a template')
     .option('--dir <dir>', 'Directory to download the template to')
     .option('-f, --force', 'Remove any existing directory or file recursively before cloning.', { default: false })
     .action(async (options) => {
-        console.log('options',options);
         const _dir = options.dir ? resolve(options.dir) : process.cwd()
         const _repo = await selectStarterTemplate(options.type)
         await download(_repo, _dir, options.force)
